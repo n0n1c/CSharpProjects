39208ce1f4caf8071e1d12f05678905d6ec8a19d
Nong <nong.suon@gmail.com>
2018-01-05 23:41:50 -0500

    finished simple webscraper

---
+++ WebScrape1/WebScrape1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScrapySharp;
+using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+
+namespace WebScrape1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var web = new HtmlWeb();
+            var page = web.Load("https://www.amazon.com/");
+
+            var items = page.DocumentNode.CssSelect("li.a-carousel-card");
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.InnerText);
+            }
+        }
+    }
+}
